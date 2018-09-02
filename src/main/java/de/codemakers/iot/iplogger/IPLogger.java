14bf9dace95c03c527a2e397a7cec069186d0de3
Paul Hagedorn <panzer1119@gmail.com>
2018-09-02 22:48:19 +0200

    Added IPLogger.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/iot/iplogger/IPLogger.java
+++ src/main/java/de/codemakers/iot/iplogger/IPLogger.java
@@ -1,9 +1,16 @@
+package de.codemakers.iot.iplogger;
 
+import de.codemakers.iot.IFTTJ;
+
+public class IPLogger {
+    
     public static final void main(String[] args) throws Exception {
         System.out.println("Test started");
-        IFTTJ.KEY = "boSS7jPNBNNVoEm8bVPB2E";
+        IFTTJ.KEY = "";
         final String ipaddress = IFTTJ.getOutInetAddress();
         IFTTJ.trigger("log_ip_address", ipaddress);
         System.out.println("Sent ip address: " + ipaddress);
         System.out.println("Test finished");
     }
+    
+}
