fadf96db364414495350c4724f4b4501b4746870
Paul Hagedorn <panzer1119@gmail.com>
2018-09-02 22:56:11 +0200

    Implemented IPLogger.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/iot/iplogger/IPLogger.java
+++ src/main/java/de/codemakers/iot/iplogger/IPLogger.java
@@ -2,15 +2,51 @@ package de.codemakers.iot.iplogger;
 
 import de.codemakers.iot.IFTTJ;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
 public class IPLogger {
     
+    public static final File FILE_PROPERTIES = new File("IPLogger.cfg");
+    
+    public static final Properties PROPERTIES = new Properties();
+    
+    static {
+        loadProperties();
+    }
+    
     public static final void main(String[] args) throws Exception {
-        System.out.println("Test started");
-        IFTTJ.KEY = "";
-        final String ipaddress = IFTTJ.getOutInetAddress();
-        IFTTJ.trigger("log_ip_address", ipaddress);
-        System.out.println("Sent ip address: " + ipaddress);
-        System.out.println("Test finished");
+        System.out.println("Started " + IPLogger.class.getSimpleName());
+        logIPAddress();
+        System.out.println("Finished " + IPLogger.class.getSimpleName());
+    }
+    
+    public static final void loadProperties() {
+        try {
+            if (!FILE_PROPERTIES.exists()) {
+                FILE_PROPERTIES.createNewFile();
+                //PROPERTIES.clear();
+                PROPERTIES.setProperty("ifttt_key", "Insert your IFTTT Webhook Key here");
+                PROPERTIES.store(new FileOutputStream(FILE_PROPERTIES, false), "Changed on:");
+            }
+            PROPERTIES.clear();
+            PROPERTIES.load(new FileInputStream(FILE_PROPERTIES));
+            IFTTJ.KEY = PROPERTIES.getProperty("ifttt_key", null);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public static final void logIPAddress() {
+        try {
+            final String ip_address = IFTTJ.getOutInetAddress();
+            IFTTJ.trigger("log_ip_address", ip_address);
+            System.out.println("Logged IP Address: " + ip_address);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
     
 }
